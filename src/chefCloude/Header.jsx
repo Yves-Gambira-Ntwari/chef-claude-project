fc6ba324d670fe37282c18a9557e85a541ceaf74
Gambira <gambirayves@gmail.com>
2026-02-12 17:42:43 +0200

    fix the bug

--- src/chefCloude/Header.jsx
+++ src/chefCloude/Header.jsx
@@ -1,9 +1,9 @@
-import LearningImg from '../img/Learning.png';
+// import LearningImg from '../img/Learning.png';
 export function Header(){
   return(
     <>
     <header>
-      <img src={LearningImg}/>
+      {/* <img src={LearningImg}/> */}
       <h1>Chef Claude</h1>
     </header>
     </>
